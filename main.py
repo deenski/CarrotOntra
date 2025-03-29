0e3fa8b20fd0aa426ef4425e4679fe8f9e35650f
Jakob Vendegna <me@jakobvendegna.dev>
2025-03-29 14:19:34 -0400

    renders background layers to screen

--- main.py
+++ main.py
@@ -8,9 +8,21 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
+pygame.display.set_caption("CarrotOntra")
+BASE_IMAGES_PATH = "assets/images"
+
+# TODO: loop through this shit and add them to an array, then blit each item in the array
+bg_img_layer_1 = pygame.image.load(f"{BASE_IMAGES_PATH}/bg_forest_layers/bg_forest_a.png").convert()
+bg_img_layer_2 = pygame.image.load(f"{BASE_IMAGES_PATH}/bg_forest_layers/bg_forest_b.png").convert_alpha()
+bg_img_layer_3 = pygame.image.load(f"{BASE_IMAGES_PATH}/bg_forest_layers/bg_forest_c.png").convert_alpha()
+bg_img_layer_2_x = 0
+bg_img_layer_3_x = 0
+bg_img_y = 0
+
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
+    screen.fill((0,0,0))
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
@@ -18,7 +30,10 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("grey")
+    screen.blit(bg_img_layer_1, (0,0))
+    screen.blit(bg_img_layer_2, (bg_img_layer_2_x, bg_img_y))
+    screen.blit(bg_img_layer_3, (bg_img_layer_3_x, bg_img_y))
+    
 
     pygame.draw.circle(screen, "yellow", player_pos, 40)
 
