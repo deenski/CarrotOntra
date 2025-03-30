efc6c0b58e15c3b26332e83e4413b4bbccca38c1
Jakob Vendegna <me@jakobvendegna.dev>
2025-03-30 08:26:17 -0400

    infinite scrolling if we want

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ logging.info("loading assets")
 # game variables
 scroll = 0
 bg_imgs = []
+level_length = 5000
 
 # load images from 0-2, 0 being furthest back
 for i in range(3):
@@ -34,16 +35,30 @@ for i in range(3):
     logging.info(bg_img.__dir__)
     bg_imgs.append(bg_img)
 
+
 bg_width = bg_imgs[0].get_width()
 
 
 # first image is furthest in the background
 def draw_bg():
-    for x in range(3):
-        speed = 1
-        for img in bg_imgs:
-            screen.blit(img, ((x * bg_width) - scroll * speed, 0))
-            speed += 0.333
+    screen_width = screen.get_width()
+    num_tiles = (screen_width // bg_width) + 2
+
+    for layer_idx, img in enumerate(bg_imgs):
+        # Assign increasing speed to background layers from back to front
+        # 1 * 0 * 0.333 = 0, 1*1*.0333 = .0333 (layer 1),
+        # 1*2*.0333 = 0.666 (foreground)
+        speed = 1 + layer_idx * 0.333 # adjust decimal to go faster or slower .1 is slower 0 is stopped
+
+        # the secret to infinite scrolling is the modulus
+        # first loop: (0 * 0) % anything = 0
+        # sixth loop: (foreground after 1 keypress): (5 * .666) % 1024 = 3.33
+        # 600th loop: (3000 * .666) % 1024 = 974
+        offset = (scroll * speed) % bg_width 
+
+        # Draw tiles
+        for x in range(-1, num_tiles):
+            screen.blit(img, ((x * bg_width) - offset, 0))
 
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
@@ -73,7 +88,7 @@ while running:
         if player_pos.x > 500:
             player_pos.x -= 300 * dt
         scroll -= 5
-    if keys[pygame.K_d] and scroll < 2600:
+    if keys[pygame.K_d] and scroll < level_length:
         logger.info("player moving to the right")
         if player_pos.x < 300:
             player_pos.x += 300 * dt
